1303f9df2fec7d2b19cbdf0d8b3453daf8d86c4c
Alan Mantoux <alan.mantoux@gmail.com>
2022-08-11 11:30:58 +0200

    API documentation update

--- proximity_screen_lock/lib/proximity_screen_lock.dart
+++ proximity_screen_lock/lib/proximity_screen_lock.dart
@@ -7,6 +7,8 @@ class ProximityScreenLock {
   /// proximity sensor.
   /// When binding is active, approaching object to proximity sensor of device
   /// will de-activate screen. When removing object, screen will be activated
+  ///
+  /// Throws error on [Android] if proximity wake lock is not supported
   static Future<void> setActive(bool value) async {
     await ProximityScreenLockPlatformInterface.instance.setActive(value);
   }
