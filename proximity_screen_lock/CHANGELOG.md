3a1e32b4465214bd6359606b8d6233fa15bfe497
Alan Mantoux <alan.mantoux@gmail.com>
2022-08-11 10:55:07 +0200

    API documentation

--- proximity_screen_lock/CHANGELOG.md
+++ proximity_screen_lock/CHANGELOG.md
@@ -1,3 +1,7 @@
+## 1.0.1
+
+* API documentation
+
 ## 1.0.0
 
 * First release
--- proximity_screen_lock/lib/proximity_screen_lock.dart
+++ proximity_screen_lock/lib/proximity_screen_lock.dart
@@ -3,10 +3,15 @@ import 'package:proximity_screen_lock_platform_interface/proximity_screen_lock_p
 class ProximityScreenLock {
   const ProximityScreenLock._();
 
+  /// If supported, setting to `true` will active bind of screen activation to
+  /// proximity sensor.
+  /// When binding is active, approaching object to proximity sensor of device
+  /// will de-activate screen. When removing object, screen will be activated
   static Future<void> setActive(bool value) async {
     await ProximityScreenLockPlatformInterface.instance.setActive(value);
   }
 
+  /// Checks if binding of screen activation to proximity sensor is supported
   static Future<bool> isProximityLockSupported() async {
     return await ProximityScreenLockPlatformInterface.instance
         .isProximityLockSupported();
