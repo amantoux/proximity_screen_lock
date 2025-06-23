b9bb20de039f8ff5ac33ac7bb138040e732c5868
Alan Mantoux <alan.mantoux@gmail.com>
2025-06-23 19:49:39 +0200

    Platform interface: Support proximity

--- proximity_screen_lock_platform_interface/lib/proximity_screen_lock_platform_interface.dart
+++ proximity_screen_lock_platform_interface/lib/proximity_screen_lock_platform_interface.dart
@@ -20,11 +20,18 @@ abstract class ProximityScreenLockPlatformInterface extends PlatformInterface {
 
   /// If set to `true` will de-activate screen if proximity sensor detects something
   /// For [Android], if used and *proximity wake lock* is not available, this will throw
+  ///
+  /// If proximity sensor is already active (resp. inactive) setting value to
+  /// `true` (resp. `false`) has no effect.
   Future<void> setActive(bool value);
 
   /// Will always return `true` except for [Android] devices for which will return
   /// if *proximity wake lock* is supported
   Future<bool> isProximityLockSupported();
+
+  /// Subscribe to a stream of proximity sensor events.
+  /// `true` is emitted if proximity sensor detects something; `false` otherwise
+  Stream<bool> get proximityStates;
 }
 
 class _DefaultProximityScreenLock extends ProximityScreenLockPlatformInterface {
@@ -36,5 +43,10 @@ class _DefaultProximityScreenLock extends ProximityScreenLockPlatformInterface {
   @override
   Future<bool> isProximityLockSupported() async {
     return false;
+  }
+
+  @override
+  Stream<bool> get proximityStates {
+    return Stream.empty();
   }
 }
